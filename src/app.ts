f0b656e9e77666d88b6bddf413f2856e9ebc6533
diegomompo7 <diego.mompo.redoli@gmail.com>
2024-01-03 19:21:33 +0100

    aws

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { configureRoutes } from "./controllers";
+import express from 'express';
+import serverless from 'serverless-http';
 
-const express = require('express')
 
 
 // Configuración del server
@@ -8,4 +9,14 @@ export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(404).send();
+  });
+  
+  app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    res.status(err.status || 500).send();
+  });
+
+  
+export const handler = serverless(app);
 configureRoutes(app);
