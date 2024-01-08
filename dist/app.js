058e269a85fadb9f7cb5508a1ba75b740b81bb3d
diegomompo7 <diego.mompo.redoli@gmail.com>
2024-01-08 10:42:14 +0100

    Cors

--- dist/app.js
+++ dist/app.js
@@ -6,11 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const controllers_1 = require("./controllers");
 const express_1 = __importDefault(require("express"));
-const port = 3000;
+const cors = require('cors');
+const port = 3001;
 // Configuración del server
 exports.app = (0, express_1.default)();
+exports.app.use(cors());
 exports.app.use(express_1.default.json());
 exports.app.use(express_1.default.urlencoded({ extended: false }));
+exports.app.get('/', function (_req, res, _next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' });
+});
 exports.app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,19 @@
 import { configureRoutes } from "./controllers";
 import express from 'express';
+const cors = require('cors');
 
-const port = 3000
+const port = 3001
+
 
 // Configuración del server
 export const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/', function (_req, res, _next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
