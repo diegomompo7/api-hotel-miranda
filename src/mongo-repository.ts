b89187bf5b55b760043be67e0596783442802c58
diegomompo7 <diego.mompo.redoli@gmail.com>
2024-04-08 15:50:30 +0200

    fix yml

--- src/mongo-repository.ts
+++ src/mongo-repository.ts
@@ -4,17 +4,17 @@ const dotenv = require('dotenv');
 // Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
-async function mongoConnect() {
-  try {
-      // Conectarse a la base de datos de Django
-      await mongoose.connect('mongodb://15.188.49.158/admin', {
-          useNewUrlParser: true,
-          useUnifiedTopology: true
-      });
-      console.log('Conexión exitosa a la base de datos de Django');
-  } catch (error) {
-      console.error(error);
-      console.log("Error en la conexión, intentando conectar en 5s...");
-      setTimeout(mongoConnect, 5000);
-  }
+export async function mongoConnect() {
+    try {
+        // Conectarse a la base de datos de Django
+        await mongoose.connect('mongodb://15.188.49.158/admin', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Conexión exitosa a la base de datos de Django');
+    } catch (error) {
+        console.error(error);
+        console.log("Error en la conexión, intentando conectar en 5s...");
+        setTimeout(mongoConnect, 5000);
+    }
 }
